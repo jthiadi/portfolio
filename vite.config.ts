7c7447043e74c7eb352f4066d263207b9a8acd57
justin <jthiadi@gapp.nthu.edu.tw>
2026-01-01 16:41:59 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio/", 
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()], // Removed componentTagger() and mode check
+  plugins: [react()], 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
